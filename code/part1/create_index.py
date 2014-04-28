84e1dc7a24e9f3aa10b6f7d38aa1bc10221548a4
Jonathan Schear <jonathanschear@gmail.com>
2014-04-28 15:59:39 -0400

    Abstracts tokenizer, adds tmp directory

--- code/part1/create_index.py
+++ code/part1/create_index.py
@@ -5,7 +5,7 @@ import csv
 import re
 
 from collections import defaultdict
-from porter_stemmer import PorterStemmer
+from tokenizer import Tokenizer
 
 def main():
     ## args
@@ -15,45 +15,25 @@ def main():
     parser.add_argument('-s', '--stop', required=True, help='Stopword list')
     opts = parser.parse_args()
 
-    '''
-    Review json format
-    {
-        'type': 'review',
-        'business_id': (encrypted business id),
-        'user_id': (encrypted user id),
-        'stars': (star rating, rounded to half-stars),
-        'text': (review text),
-        'date': (date, formatted like '2012-03-14'),
-        'votes': {(vote type): (count)},
-    }
-    '''
-
     ## Output file
     csv_writer = csv.writer(open(opts.out, 'w'), delimiter="\t")
     csv_writer.writerow(['token', 'business_id', 'review_id', 'position', '...'])
 
-    ## Stemmer, stopwords, dict
-    stopwords = set(line.strip() for line in open(opts.stop)) # Create stopword set
-    stemmer = PorterStemmer() # init Stemmer
+    ## Tokenizer
+    tk = Tokenizer(opts.stop)
     token_map = defaultdict(list)
 
     ## Tokenize review texts
-    # for each word in the vocabulary (in this case all words found in all reviews)
-    # you should be able to retrieve a list of postings containing:
+    # for each word in the vocabulary (in this case all words found in all reviews):
     # business id, review id, and position of each term occurrence
-    # instead of using the review id, use the line on which the review occurs as a unique identifier
-
+    # instead of using the review id, uses the line on which the review occurs as a unique identifier
     reviews = open(opts.reviews)
     for review_num, line in enumerate(reviews):
         review = json.loads(line)
         business_id = review['business_id'].encode('utf-8')
-        text = review['text'].lower() # lowercase
-
-        for position, word in enumerate(text.split()):
-            word = re.sub(r'[^\w\s]', '', word)
-            word = stemmer.stem(word, 0, len(word) - 1) # apply stemming
-            if word not in stopwords and word != '': # filter stopwords
-                token_map[word].append((business_id, review_num, position))
+        tokens = tk.tokenize(review['text'])
+        for position, word in enumerate(tokens):
+            token_map[word].append((business_id, review_num, position))
 
     ## Print sorted inverted index
     for token in sorted(token_map):
---
+++ code/part1/tokenizer.py
@@ -0,0 +1,28 @@
+from porter_stemmer import PorterStemmer
+import re
+import string
+
+class Tokenizer(object):
+    def __init__(self, stopwords_file):
+        self.stemmer = PorterStemmer()
+        self.stop_words = set(line.strip() for line in open(stopwords_file))
+
+    def tokenize(self, text):
+        tokens = []
+        words = text.lower().split()
+        for word in words:
+            word = re.sub(r'[^\w\s]', '', word) # remove punctuation
+            word = self.stemmer.stem(word, 0, len(word) - 1) # stem
+
+            # throw out words in stop words and those starting with non alphabet
+            if word and word not in self.stop_words and word[0].isalpha():
+                tokens.append(word)
+
+        return tokens
+
+## Possible thing to add: replace 3 or more occurrences of the same character with two occurrences
+# for sub_word in word.split():
+#     # Replace three or more occurrences of the same character with two occurrences
+#     unique_chars = ''.join(set(sub_word))
+#     for c in unique_chars:
+#         sub_word = re.sub(c + r'{3,}', c + c, sub_word)
