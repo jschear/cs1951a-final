e531944693122ae4e61a5201bb747ac7cf960531
Jonathan Schear <jonathanschear@gmail.com>
2014-05-09 21:18:11 -0400

    Finished word clouds, cleaned up part one code

--- code/part1/query_index.py
+++ code/part1/query_index.py
@@ -12,29 +12,23 @@ import math
 class UninformativeQueryException(Exception):
     pass
 
-class SearchEngineData(): #lazy access dictionary on disk, takes in an index, and a file descriptor (that stays open, yikes)
-    def __init__(self, index, path, to_cache = {}): 
+class SearchEngineData():
+    #lazy access dictionary on disk, takes in an index, and a file descriptor (that stays open, yikes)
+    def __init__(self, index, path, to_cache = {}):
         self.data = defaultdict(lambda: defaultdict(list))
         self.index = index
-        # print to_cache, len(to_cache)
-        # pdb.set_trace()
         self.f = open(path,"r")
-        for i,item in enumerate(to_cache):
-            # print i
-            self.__getitem__(item)
+        for item in enumerate(to_cache):
+            self.__getitem__(i)
 
-    def __getitem__(self, key): #returns 
-        # pdb.set_trace()
-       
+    def __getitem__(self, key):
         if key in self.data:
             return self.data[key]
-        # print "getting" + key
         if key not in self.index:
             return defaultdict(list)
         pos = self.index[key]
         self.f.seek(pos)
         line = self.f.readline()
-        # print "processing"
         return self.process_line(line)
 
     def process_line(self, line):
@@ -43,23 +37,14 @@ class SearchEngineData(): #lazy access dictionary on disk, takes in an index, an
         for item in row[1:]:
             businessID, review_num, position = eval(item) #which is review_num, and which is position again?
             self.data[token][businessID].append((review_num, position))
-        # pdb.set_trace()
-        # print "done processing"
         return self.data[token]
 
-                    # self.data[token].append((businessID,review_num,position))  #4165709824B total usage
-
-
-
-    # def __setitem__(self, key):
 
 class SearchEngine:
     def __init__(self, index_file, tokenizer, business_file, num_results = 10, in_memory = False):
         self.tokenizer = tokenizer
         self.fields_to_display = ['name','business_id','full_address','stars','review_count','categories']
         self.CACHE_THRESHOLD = 100000000
-        # self.data = defaultdict(lambda : defaultdict(list))
-        # self.data = defaultdict(list)
         self.data = None
         self.num_results = num_results
         self.num_businesses = 0
@@ -82,7 +67,7 @@ class SearchEngine:
                     relevant_data[item] = doc[item]
 
                 self.business_data[business_id] = relevant_data
-                # pdb.set_trace()
+
             self.num_businesses = len(self.business_data)
 
     def read_data(self, index_file):
@@ -115,35 +100,9 @@ class SearchEngine:
                 index[token] = currindex
                 currindex = f.tell()
 
-
             self.data = SearchEngineData(index, index_file,to_cache = to_cache)
 
-
-
-
-        
-                    # self.data[token].append((businessID,review_num,position))  #4165709824B total usage
-            
-            # for line in reviews:
-            #     self.ratings[line] = json.loads(line)['stars']
-            # pdb.set_trace()
-            # pdb.set_trace()
-       
-            # pdb.set_trace()
-
-
-            
-                # self.data[token] = [eval(item) for item in row]
-
-
-
         print "processed"
-
-    # def make_doc_dict(self,tuples): #takes the tuples of the form (businessID, review, frequency), outputs them to a dictionary like this: business : reviewID : list of positions 
-    #     out = defaultdict(list)
-    #     for businessID, review_num, position in tuples:
-    #         out[businessID].append((review_num,position))
-    #     return out
 
 
     def process_query(self,query,html = False):
@@ -169,35 +128,20 @@ class SearchEngine:
                 return str(field_name) + ": " + str(value)
 
     def htmlrep_item(self, field_name, value):
-            # field_name, value = item
             if field_name == "categories":
                 return "<em> " + str(field_name) + "</em> : " + ", ".join(map(str, value))
             else:
                 return "<em> " + str(field_name) + "</em> : " + str(value)
 
     def strrep_business(self, business_id, strrep_item):
-        
         data = self.business_data[business_id]
-        # pdb.set_trace()
         fields = map(data.get, self.fields_to_display)
-        # print zip(self.fields_to_display,fields)
         out = "\n".join(map(lambda item: strrep_item(*item), zip(self.fields_to_display, fields)))
-        # out = "Name : " + name + "\nRating: " + str(stars) + "\nReview Count: " + str(review_count) + "\nBusinessID: " + business_id 
         out += "\n"
-        # try:
-        #     out = """
-        #         Name: {name} 
-        #         Rating: {stars}
-        #         Review Count: {review_count}
-        #         Business ID: {business_id}
-        #         """.format(**data)
-        # except Exception:
-        #     pdb.set_trace()
         return out
 
 
     def strrep_results(self, business_ids):
-        # pdb.set_trace()
         return "\n".join(map(lambda business: self.strrep_business(business, self.strrep_item),business_ids))
 
     def htmlrep_results(self, business_ids):
@@ -209,7 +153,6 @@ class SearchEngine:
 
 
     def tf_idf(self, business_id, token):
-        # pdb.set_trace()
         tf = len(self.data[token][business_id])
         idf = math.log(self.num_businesses/len(self.data[token]))
         return tf*idf
@@ -227,7 +170,6 @@ class SearchEngine:
         rating = self.get_rating(business_id)
         num_reviews = self.get_review_count(business_id)
         return rating*math.log(num_reviews)*sum(map(lambda term: self.tf_idf(business_id,term),terms))
-        # return sum(map(lambda term: self.tf_idf(business_id,term),terms))
 
     def rank_results(self, business_ids, terms):
         return sorted(business_ids, key = lambda b: self.ranking_function(b,terms),reverse = True)
@@ -251,31 +193,18 @@ class SearchEngine:
             return self.one_word_query(tokenized[0])
         else:
             return self.free_text_query(tokenized)
-        # return {"OWQ" : one_word_query, "FTQ" : free_text_query, "PQ" : phrase_query}[self.query_type(query)](query)
 
-    
 
     def one_word_query(self,tokenized_word):
-        return [business_id for business_id in self.data[tokenized_word]], [tokenized_word] 
+        return [business_id for business_id in self.data[tokenized_word]], [tokenized_word]
 
     def free_text_query(self,tokenized_line):
         return list(reduce(operator.and_ ,map(lambda token: self.data[token].viewkeys(), tokenized_line))), tokenized_line
 
     def phrase_query(self, tokenized_line):
         business_ids, _ = self.free_text_query(tokenized_line)
-        # print len(business_ids)
         out = filter(lambda business_id: self.contains_phrase(business_id, tokenized_line), business_ids)
-        # pdb.set_trace()
-        # print len(out)
-
         return out, tokenized_line
-
-    # def contains_phrase(self, business_id, tokenized_line):
-    #     # pdb.set_trace()
-    #     # for offset, word in enumerate(tokenized_line):
-    #     #     pos_set = _offset_position_set(word,business_id, offset) #set of 
-    #     pos_sets = [self._offset_position_set(word, business_id, offset) for offset, word in enumerate(tokenized_line)]
-    #     return filter(lambda set_: len(set_) > 0, pos_sets)
 
     def contains_phrase(self, business_id, tokenized_line):
         pos_sets = [self._offset_position_dict(word, business_id, offset) for offset, word in enumerate(tokenized_line)] #list of dict(review -> (pos...)) lists
@@ -298,40 +227,21 @@ class SearchEngine:
                 return True
         return False
 
-        # for pos_set in pos_sets: #for every word in the phrase
-        #     for review in common_reviews
-            
-
-
 
     def set_intersect(self, sets):
         return reduce(operator.and_, sets)
 
     def dict_intersect(self, dicts): #gets the list of keys for a given set
-        # pdb.set_trace()
         return reduce(lambda d1, d2: set(d1) and set(d2), dicts)
-    # def common_element(self, sets):
-        # if len(sets) == 1:
-        #     return True
-        # else:
-        #     starter, rest = sets[0], sets[1:]
-        #     for item in starter:
-        #         for set_ in rest:
-        #             if item not in set_:
-        #                 break
 
-
-
-    def _offset_position_dict(self, word, business_id, offset): #turns a list of (review_num, position) [corresponding to a business_id and word], to a map from review_num to a set of positions - offset (to check consecutiveness)
+    def _offset_position_dict(self, word, business_id, offset):
+    #turns a list of (review_num, position) [corresponding to a business_id and word],
+    # to a map from review_num to a set of positions - offset (to check consecutiveness)
         index_list = self.data[word][business_id]
         out = defaultdict(set)
         for review_num, position in index_list:
             out[review_num].add(position-offset)
-        # pdb.set_trace()
-
         return out
-
-
 
 
 
@@ -345,7 +255,6 @@ def main():
     parser.add_argument('-m', '--in_memory', action = "store_true", help = 'Flag to store all the data in memory. Increases program speed memory consumption, decreases the startup speed.')
     parser.add_argument('-html', '--htmlrep', action = "store_true", help = 'Display results as an html list')
     opts = parser.parse_args()
-    # pdb.set_trace()
     tokenizer = Tokenizer(opts.stop)
     search_engine = SearchEngine(opts.index, tokenizer, opts.businesses, num_results = 10, in_memory = bool(opts.in_memory))
     while True:
